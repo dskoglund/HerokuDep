fa1584ef89a738cb75828efc35b7412ac32e2d6c
David Skoglund <dskoggz@gmail.com>
2016-10-19 10:08:30 -0700

    Deploy.

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const bookRouter = require('./book-routes')
 const MongoClient = require('mongodb').MongoClient
 const createApp = require('./create-app')
 
-const MONGO_URI = 'mongodb://localhost:27017/test-books'
+const DEFAULT_MONGO_URI = 'mongodb://localhost:27017/mongodb-express-travis-heroku'
+const NODE_ENV = process.env.NODE_ENV || 'developement'
+const MONGODB_URI = process.env.MONGODB_URI || DEFAULT_MONGO_URI
 
-MongoClient.connect(MONGO_URI, (err, db) => {
+const PORT = process.env.PORT || 3000
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
 
   const app = createApp(db)
 
-  app.listen(3000, () => {
+  app.listen(PORT, () => {
+    NODE_ENV !== 'production' &&
     console.log('listening.......')
   })
 })
